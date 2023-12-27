c6178a2e7908565f96c2c07447707cf70589c52e
yNOtOmosh <onyango.omondi.tony@gmail.com>
2023-12-27 01:48:06 -0800

    code style change

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,12 +1,18 @@
+#!/usr/bin/python3
+"""starts a Flask web app."""
+
+
 from flask import Flask
 
 app = Flask(__name__)
+
 
 # defination with strict_slashes=False
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     return 'Hello HBNB!'
 
+
 if __name__ == '__main__':
-    # run on 0.0.0.0:5000
+    # run/listen on 0.0.0.0:5000
     app.run(host='0.0.0.0', port=5000)
